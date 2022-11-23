02df09c0554718edf47182fa4eaea4f4add37631
Galaxomas <thefidanabdulla@gmail.com>
2022-11-23 18:43:26 +0400

    Step 2 is done, begin to step 3

--- John Lemon's Haunted Jaunt/Assets/Scripts/PlayerMovement.cs
+++ John Lemon's Haunted Jaunt/Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,47 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Vector3 m_Movement;
+    public float turnSpeed = 20f;
+
+    [SerializeField]
+    private Animator m_Animator;
+    [SerializeField]
+    private Rigidbody m_Rigidbody;
+
+    private Vector3 m_Movement;
+    private Quaternion m_Rotation = Quaternion.identity;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-            
+        //m_Animator = GetComponent<Animator>();        
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+
     }
 }
